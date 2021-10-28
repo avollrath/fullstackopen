144fc4fcd226be43f95b59423fafed2b982b3b2a
André Vollrath <46895425+avollrath@users.noreply.github.com>
2021-10-28 16:32:32 +0300

    filter logic

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,77 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ newFilter, setNewFilter ] = useState('')
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-    handleFilter(newFilter)
-  }
-
-  const handleFilter = () => {
-
-  }
-
-
-
+    setNewFilter(event.target.value);
+  };
 
   const addPerson = (e) => {
-    e.preventDefault()
-    if (persons.find(person => person.name.toLowerCase() === newName.toLowerCase()))
-      alert(`${newName} is already added to phonebook`)
-    else
-      {
-        const personObject = {
-          name: newName,
-          number: newNumber,
-          id: persons.at(-1).id + 1
-        }
-        setPersons(persons.concat(personObject))
-        setNewName('')
-        setNewNumber('')
-      }
-  }
+    e.preventDefault();
+    if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase())) alert(`${newName} is already added to phonebook`);
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.at(-1).id + 1,
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
 
-  const Person = ({persons}) => {
-
-    const filteredArr = persons.filter(person => {
-      return person.name.toLowerCase().includes(newFilter.toLowerCase())})
-    if (newFilter != '')
-
-
-    return  (
-              <ul>
-                {persons.map(person =>
-                  <li key={person.id}>
-                    {person.name} : {person.number} : {person.id}
-                  </li>
-                )}
-              </ul>
-            )
-  }
+  const Person = ({ persons }) => {
+    if (newFilter !== "") {
+      persons = persons.filter((person) => {
+        return person.name.toLowerCase().includes(newFilter.toLowerCase());
+      });
+    }
+    return (
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>
+            {person.name} : {person.number} : {person.id}
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>Filter shown with:
+      <p>
+        Filter shown with:
         <input onChange={handleFilterChange} value={newFilter} />
       </p>
       <form>
@@ -82,14 +71,15 @@ const App = () => {
           number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
-          <button type="submit" onClick={addPerson}>add</button>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
         </div>
       </form>
-      <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      <Person persons={persons}/>
+      <Person persons={persons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
