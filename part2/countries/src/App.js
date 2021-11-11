a779f1cc1f046d916d1f831026128d7e637bdaf9
André Vollrath <46895425+avollrath@users.noreply.github.com>
2021-11-11 16:53:35 +0200

    languages

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,21 +4,39 @@ import axios from 'axios'
 const App = () => {
 
   const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
   const Countries = ({ countries }) => {
-
     return (
-      <ul>
+      <div>
         {countries.map((country) => (
           <div key={country.numericCode}>
             <h2>{country.name}</h2>
             <p>Capital: {country.capital}</p>
             <p>Population: {country.population}</p>
-            <p>Language(s): {country.languages[0].name}</p>
+            <p>Language(s):</p>
+            <ul>
+              {country.languages.map(lang => (
+                 <li key={lang.name}>{lang.name}</li>
+              ))}
+            </ul>
             <img src={country.flag} alt={country.name} width="300px" />
           </div>
         ))}
-      </ul>
+      </div>
+    );
+  };
+
+  const Filter = () => {
+    return (
+      <div>
+        Search countries:
+        <input onChange={handleFilterChange} value={filter} />
+      </div>
     );
   };
 
@@ -27,8 +45,6 @@ const App = () => {
     axios
       .get('https://restcountries.com/v2/all?fields=name,capital,population,languages,flag,numericCode')
       .then(response => {
-        console.log('promise fulfilled')
-        console.log(response.data)
         setCountries(response.data)
       })
   }, [])
@@ -38,6 +54,7 @@ const App = () => {
   return (
     <div className="App">
      <h1>Countries</h1>
+     <Filter />
      <Countries countries={countries} />
     </div>
   );
